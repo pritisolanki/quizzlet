842350c6eb2d88fb2aa9bb49ef430d8d12ec0417
pritisolanki <pritiatwork@gmail.com>
2023-03-12 13:54:41 -0400

    Tsk: gtag

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import ReactGA from 'react-ga';
-ReactGA.initialize('G-TKL82ZCZDH');
-ReactGA.pageview(window.location.pathname + window.location.search);
+import TagManager from 'react-gtm-module'
 
+const tagManagerArgs = {
+  gtmId: 'G-TKL82ZCZDH'
+}
+TagManager.initialize(tagManagerArgs)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
